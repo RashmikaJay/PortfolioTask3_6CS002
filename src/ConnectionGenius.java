35aca09a18a1199743b5cb056d4bfcd0399e85d1
RashmikaJay <rashiijayasinghe@gmail.com>
2021-02-05 00:23:37 +0530

    PortfolioTask3_6CS002 First commit

---
+++ src/ConnectionGenius.java
@@ -0,0 +1,16 @@
+import java.net.InetAddress;
+
+
+public class ConnectionGenius {
+
+  InetAddress ipa;
+  
+  public ConnectionGenius(InetAddress ipa) {
+    this.ipa = ipa;
+  }
+
+  
+  
+  
+
+}
